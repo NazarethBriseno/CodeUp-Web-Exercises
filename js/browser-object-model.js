087ee2d0116ab145beb660a5330de0da8cb82026
Nazareth Briseno <nazarethbriseno@gmail.com>
2022-09-14 14:57:29 -0500

    Completed Defuse the BOM

---
+++ js/browser-object-model.js
@@ -0,0 +1,26 @@
+// let sayHey =function(){
+//     let count = 0;
+//     function hey(){
+//         console.log("Hey" + count);
+//         count++;
+//     } else {
+//         clearInterval();
+//     }
+//
+//     setInterval(hey, 5000);
+// }
+
+// let count = 0;
+// let thisTimeItWillStop;
+// function hey(){
+//     if(count < 10){
+//         console.log("hey " + count);
+//         count++
+//     } else {
+//         clearInterval(thisTimeItWillStop);
+//     }
+// }
+// thisTimeItWillStop = setInterval(hey, 1000)
+
+// let answer = confirm("How are you?");
+// alert(answer);
