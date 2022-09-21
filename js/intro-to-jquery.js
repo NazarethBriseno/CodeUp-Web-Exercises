e813d07a8b264dc27d874bec27d1a5dc59b63668
Nazareth Briseno <nazarethbriseno@gmail.com>
2022-09-21 12:45:52 -0500

    Completed third jQuery Exercise

--- js/intro-to-jquery.js
+++ js/intro-to-jquery.js
@@ -8,20 +8,53 @@
 // });
 
 
-let $refToLibrary = $("#library");
+// let $refToLibrary = $("#library");
 //ID Selector to alter content
 // $refToLibrary.text("jQuery makes life easier");
 
 //Get reference to content
-console.log($refToLibrary.text());
+// console.log($refToLibrary.text());
 
-let li3text = $refToLibrary.text();
-$refToLibrary.text(li3text + " makes life easier");
+// let li3text = $refToLibrary.text();
+// $refToLibrary.text(li3text + " makes life easier");
+//
+// $(".framework").css({
+//     'border': '1px solid black',
+//     'width': 'fit content'
+// });
+//
+// $("p, li").text("I've taken over.").css("color", "hotpink")
 
-$(".framework").css({
-    'border': '1px solid black',
-    'width': 'fit content'
+$(function(){
+
+    $('#clickMe').on('click', function(){
+        alert('yo');
+    });
+
+    $("#create-an-element").on('click', function(){
+        $(this).after("<button class='will-this-work'>Click Me</button>");
+        })
+
+    $("#dynamic-content").on('click', '.will-this-work', function(){
+        alert("Yo!");
+    });
+
+    $(".box").hover(function(){
+            $(this).addClass("hotpink");
+        },
+        function(){
+            $(this).removeClass("hotpink");
+        });
+
 });
 
-$("p, li").text("I've taken over.").css("color", "hotpink")
+    //this wont work because we are trying to register an even handler with an element that doesnt exist yet
+    // $(".will-this-work").on('click', function(){
+    //     alert("YO");
+    // })
+
+    //This will work
+//Delegated event
+
+
 
