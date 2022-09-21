ba394920604a5e50784d1f4b0966168c18a08ae9
Nazareth Briseno <nazarethbriseno@gmail.com>
2022-09-21 09:57:30 -0500

    Finished Second jQuery Exercise

--- js/intro-to-jquery.js
+++ js/intro-to-jquery.js
@@ -7,3 +7,21 @@
 //     });
 // });
 
+
+let $refToLibrary = $("#library");
+//ID Selector to alter content
+// $refToLibrary.text("jQuery makes life easier");
+
+//Get reference to content
+console.log($refToLibrary.text());
+
+let li3text = $refToLibrary.text();
+$refToLibrary.text(li3text + " makes life easier");
+
+$(".framework").css({
+    'border': '1px solid black',
+    'width': 'fit content'
+});
+
+$("p, li").text("I've taken over.").css("color", "hotpink")
+
