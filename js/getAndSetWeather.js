a8e4258f66abfebb6355476cfe5ed5e6d6becf35
Nazareth Briseno <nazarethbriseno@gmail.com>
2022-09-29 14:14:48 -0500

    Finished es6 Exercises

--- js/getAndSetWeather.js
+++ js/getAndSetWeather.js
@@ -1,3 +1,4 @@
+//This function puts in the default weather cards for San Antonio
 function getAndSetWeather(){
     //Weather API
     $.get("http://api.openweathermap.org/data/2.5/weather", {
@@ -57,8 +58,7 @@ function getAndSetWeather(){
         }
     }); // end forecast ajax request and .done
 }
-
-
+//This function empty's the previous weatherCards div and pushes updated weather cards when the button is pushed
 function changeWeather(result){
     $.get("http://api.openweathermap.org/data/2.5/weather", {
         APPID: OPEN_WEATHER_APPID,
@@ -90,6 +90,65 @@ function changeWeather(result){
         APPID: OPEN_WEATHER_APPID,
         lat: result[1],
         lon: result[0],
+        units: "imperial"
+    }).done(function (data) {
+        console.log(data);
+        console.log("FORECAST");
+        //This for Loop takes the string with the date and time and splits it, so if the object is equal to zero, it
+        // will append everything.
+        for (let i = 0; i < data.list.length; i++) {
+            let seperatedDateAndTime = data.list[i].dt_txt.split(" ");
+            if (seperatedDateAndTime[1] === "00:00:00") {
+                $("#weatherCards").append(`
+                <div class="card text-center m-2" style="width: 14rem;">
+                    <div class="card-header">
+                        ${seperatedDateAndTime[0]}<img src="http://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png">
+                    </div>
+                    <ul class="list-group list-group-flush">
+                       <li class="list-group-item">Temperature is ${data.list[i].main.temp}</li>
+                       <li class="list-group-item">Feels like ${data.list[i].main.feels_like}</li>
+                       <li class="list-group-item">Description: ${data.list[i].weather[0].description}</li>
+                       <li class="list-group-item">Humidity: ${data.list[i].main.humidity}</li>
+                       <li class="list-group-item">Pressure: ${data.list[i].main.pressure}</li>
+                     </ul>
+                </div>
+            `)
+            }
+        }
+    }); // end forecast ajax request and .done
+}
+//This function empty's the previous weatherCards div and pushes updated weather cards when the pin on the map is moved
+function hoverWeather(lngLat){
+    $.get("http://api.openweathermap.org/data/2.5/weather", {
+        APPID: OPEN_WEATHER_APPID,
+        lat: lngLat.lat,
+        lon: lngLat.lng,
+        units: "imperial"
+    }).done(function (data) {
+        console.log(data);
+        console.log("WEATHER");
+        $("#weatherCards").empty().append(`
+       <div class="card text-center m-2" style="width: 14rem;">
+          <div class="card-header">
+            Today's Weather<img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png">
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">Temperature is ${data.main.temp}</li>
+            <li class="list-group-item">Feels like ${data.main.feels_like}</li>
+            <li class="list-group-item">Description: ${data.weather[0].description}</li>
+            <li class="list-group-item">Humidity: ${data.main.humidity}</li>
+            <li class="list-group-item">Pressure: ${data.main.pressure}</li>
+          </ul>
+       </div>
+    `)
+    });
+
+
+//ForeCast
+    $.get("http://api.openweathermap.org/data/2.5/forecast", {
+        APPID: OPEN_WEATHER_APPID,
+        lat: lngLat.lat,
+        lon: lngLat.lng,
         units: "imperial"
     }).done(function (data) {
         console.log(data);
--- js/weather_map.js
+++ js/weather_map.js
@@ -1,16 +1,26 @@
 $(function(){
-
+    //This function calls to the default weather which is set to San Antonio
     getAndSetWeather();
 
     //MapBOX
     mapboxgl.accessToken = 'pk.eyJ1IjoibmF6YXJldGhicmlzZW5vIiwiYSI6ImNsOGVwczdtZzAwdGIzcW1tbGtwcGd3OWkifQ.As2ZN5u4cqcPgsQtgcdZJQ';
     const map = new mapboxgl.Map({
         container: 'map', // container ID
-        style: 'mapbox://styles/mapbox/streets-v11', // style URL
+        style: 'mapbox://styles/mapbox/dark-v10', // style URL
         center: [-74.5, 40], // starting position [lng, lat]
         zoom: 9, // starting zoom
         projection: 'globe' // display the map as a 3D globe
-    });
+                                });
+    map.addControl(new mapboxgl.NavigationControl());
+    map.addControl(
+        new mapboxgl.GeolocateControl({
+            positionOptions: {
+                enableHighAccuracy: true
+                              },
+            trackUserLocation: true,
+            showUserHeading: true
+                                      })
+                    );
 
     $("#submitMarker").on('click', function(){
         geocode($("#userMarker").val(), MAPBOX_API_TOKEN).then(function(result) {
@@ -20,15 +30,17 @@ $(function(){
                 })
                 .setLngLat(result)
                 .addTo(map);
-                console.log(result[0]);
 
+                console.log(result[0]);
+                changeWeather(result);
                 function onDragEnd() {
                     const lngLat = marker.getLngLat();
                     coordinates.style.display = 'block';
                     coordinates.innerHTML = `Longitude: ${lngLat.lng}<br />Latitude: ${lngLat.lat}`;
+                    hoverWeather(lngLat);
                 }
                 marker.on('dragend', onDragEnd);
-                changeWeather(result);
+
         }) //End of GeoCode result function
 
     }); // end submit marker click event handler
